53e39abf8e3b867210dad975d58182498c3047b7
Pedro <pedropaulo91.calazans@gmail.com>
2023-02-08 19:40:32 -0300

    Criação dos componentes + novos pacotes + css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-
-const GET_AULAS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-
-  const { data} = useQuery<{lessons: Lesson[]}>(GET_AULAS_QUERY)
-
-
-      console.log(data);
-  
-
   return (
-   <ul>
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title}</li>
-    })}
-   </ul>
+    <Event />
   )
 }
 
 export default App
-
-
-
-
---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export function Logo() {
+    return(
+        <img src="../ignite.png" alt="logo" style={{width: "10vw", height: "5vh"}}/>
+    )
+}
