77914f560ecde2a6d1dd932d08604b3009fa178e
Pedro <pedropaulo91.calazans@gmail.com>
2023-02-10 21:41:21 -0300

    Finalizando componente Descrição + término do projeto faltando configurar apenas o codegen.

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,7 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 interface LessonProps {
     title: string;
@@ -11,11 +11,14 @@ interface LessonProps {
 }
 
 export function LessonCard(props: LessonProps) {
+    const { slug } = useParams<{ slug: string }>()
+
     const isLessonDisponivel = isPast(props.dataDisponibilidade);
     const dataFormatada = format(props.dataDisponibilidade, "EEEE' - 'd' de 'MMMM' - 'k'h'mm", {
         locale: ptBR,
     })
 
+    const isActiveLesson = slug === props.slug;
 
     return (
         <Link to={`/event/lesson/${props.slug}`} className="group">
@@ -23,10 +26,14 @@ export function LessonCard(props: LessonProps) {
                 { dataFormatada }
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+            <div 
+                className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 
+                    'bg-green-500' : ''}`}
+            >
                 <header className="flex items-center justify-between">
                     {isLessonDisponivel ? (
-                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                        <span className={`text-sm font-medium flex items-center gap-2 ${isActiveLesson ? 
+                        'text-white' : 'text-blue-500'}`}>
                             <CheckCircle size={20} />
                             Conteúdo liberado
                         </span>
@@ -41,7 +48,7 @@ export function LessonCard(props: LessonProps) {
                     </span>
                 </header>
 
-                <strong className="text-gray-200 mt-5 block">
+                <strong className={`mt-5 block ${isActiveLesson ? 'text-white' : 'text-gray-200'}`}>
                    { props.title }
                 </strong>
             </div>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.hygraph.com/v2/cld98ojzd0kf001ujdb57ge5u/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
 
